c3a8b62b111431a3c45b72bdbd90957a049dc4e3
Deszip <raf.rafiki@gmail.com>
2020-11-11 20:55:22 +0200

    Dropped e2e version from SPM

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
             name: "AppSpectorSDK",
             targets: ["AppSpectorSDK"]),
         .library(
-            name: "AppSpectorSDKE2E",
-            targets: ["AppSpectorSDKE2E"]),
-        .library(
             name: "AppSpectorTVSDK",
             targets: ["AppSpectorTVSDK"])
     ],
@@ -22,11 +19,6 @@ let package = Package(
             name: "AppSpectorSDK",
             url: "https://github.com/appspector/ios-sdk/releases/download/1.3.7/AppSpectorSDK.xcframework.zip",
             checksum: "f00e30664b5854712abcc2e923575fa63e5b2e415e99d0212512ce751805a1a8"
-        ),
-        .binaryTarget(
-            name: "AppSpectorSDKE2E",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.3.7/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "a6a001fc6644164c53ef2f8a3c0ac6ccdc1f1355ccaeca474baeb0415076fa89"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
