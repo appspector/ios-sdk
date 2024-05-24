f155319e75731baf45e764447a84581cbd24f013
Deszip <deszip@DuckBox.local>
2024-05-24 22:20:41 +0300

    SDK update: v 1.4.12 (1.4.12)

--- Package.swift
+++ Package.swift
@@ -14,23 +14,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSpectorSDK",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.11/AppSpectorSDK.xcframework.zip",
-            checksum: "78c15a6c217b67f1537565f22188a5555e43ff534b6e8c4cd83d5e4de4104462"
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.12/AppSpectorSDK.xcframework.zip",
+            checksum: "d153cf89ad87e0780de68c13fbd9c93bd2b5d80e960967392d3d54f1c2252c3f"
         ),
         .binaryTarget(
             name: "AppSpectorSDKE2E",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.11/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "cf070feecd41ad8febd6c01118729b069df0dd765cbd1b6db4ddd67f54a76428"
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.12/AppSpectorSDKE2E.xcframework.zip",
+            checksum: "1397a0785d3d79a632193ad222e11936a8b7a97e7dc40ac715ae34fa68364dc6"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.11/AppSpectorTVSDK.xcframework.zip",
-            checksum: "e3b1d949b828344fcd7a035b9a45492a607723fb0e44eb45c9968c7b5ff34405"
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.12/AppSpectorTVSDK.xcframework.zip",
+            checksum: "fdccbfc5432adadf7f93bb936264acaca7e16619e11e16912f9017145e2f177d"
         ),
         .binaryTarget(
             name: "AppSpectorOSXSDK",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.11/AppSpectorOSXSDK.xcframework.zip",
-            checksum: "54eda74111b0b28b71c0821d4d2b579dc476c5de8e7f2daa5211cc4ae39f96bf"
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.12/AppSpectorOSXSDK.xcframework.zip",
+            checksum: "474a361e7b7a5d7449799fd5566c1123bb91cf1ac07407f8001e498cca2e3a51"
         )
     ]
 )
